c3cabe975cfa71e0ba8ae0eda8fdb6fe53871f82
rncraven94 <neilcraven2013@gmail.com>
2024-01-25 22:04:08 -0500

    index.html

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ use: "strict";
 console.log(12);
 
 //days of the week
+const dayHeading = document.getElementById("dayHeading");
 
 const currentDate = new Date();
 const dayOfWeek = currentDate.getDay();
@@ -16,3 +17,7 @@ const weekdays = [
   "Saturday",
 ];
 console.log(weekdays[dayOfWeek]);
+
+document.addEventListener("DOMContentLoaded", function () {
+  dayHeading.textContent = `Today is ${weekdays[dayOfWeek]}`;
+});
