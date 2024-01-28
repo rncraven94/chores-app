74fe8cd0aedf8c9e75404b6036ceb4da0522af9a
rncraven94 <neilcraven2013@gmail.com>
2024-01-28 18:17:14 -0500

    index.html

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ console.log(12);
 
 //days of the week
 const dayHeading = document.getElementById("dayHeading");
+const choreList = document.getElementById("choreList");
 
 const currentDate = new Date();
 const dayOfWeek = currentDate.getDay();
@@ -35,4 +36,5 @@ console.log(weekdays[dayOfWeek].name);
 
 document.addEventListener("DOMContentLoaded", function () {
   dayHeading.textContent = `Today is ${weekdays[dayOfWeek].name}`;
+  choreList.textContent = `${weekdays[dayOfWeek].chores}`;
 });
